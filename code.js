4fc62432431c2af70624987ff31048f9bc957b86
Yazeed Asaad <159422722+YazeedAsaad01@users.noreply.github.com>
2024-02-12 21:01:00 -0800

    Update code.js
    added comments

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+// Define an array containing numbers
 let numbers = [
     10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 
     11, 22, 33, 44, 55, 66, 77, 88, 99, 110,
@@ -7,17 +8,18 @@ let numbers = [
     616, 748, 385, 534, 215, 947, 793, 469, 95, 510
 ];
 
+// Define an empty array to store even numbers
 let even = [];
 
+// Define a function to find even numbers in an array
 function findTheEvenNumber(array) {
     for(let i = 0; i < array.length; i++) {
-        if(i % 2 == 0) {
-            even.push(array[i]);
+        if(i % 2 == 0) { // Check if the index is even
+            even.push(array[i]); // Push the even number to the 'even' array
         }
     }
-    return even;
+    return even; // Return the array containing even numbers
 }
 
+// Call the function to find and output even numbers in the 'numbers' array
 console.log(findTheEvenNumber(numbers));
-
-
